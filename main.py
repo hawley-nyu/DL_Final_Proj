14f029ca35912cefefa4213c4e4b27b1e5eec43e
Zhiyu Guo <zg915@nyu.edu>
2024-11-24 18:42:20 -0500

    what does dataloader look like

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import glob
 def get_device():
     """Check for GPU availability."""
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Using device: ", device)
+    print("Using device:", device)
     return device
 
 
---
+++ train_model.py
@@ -0,0 +1,30 @@
+from dataset import create_wall_dataloader
+import torch
+
+def get_device():
+    """Check for GPU availability."""
+    if torch.backends.mps.is_available(): device = torch.device("mps")
+    elif torch.cuda.is_available(): device = torch.device("cuda")
+    else: device = torch.device("cpu")
+    print("Using device:", device)
+    return device
+
+
+def load_data(device):
+    data_path = "/scratch/DL24FA"
+
+    train_ds = create_wall_dataloader(
+        data_path=f"{data_path}/train",
+        probing=False,
+        device=device,
+        train=True,
+    )
+
+    return train_ds
+
+if __name__ == "__main__":
+    device = get_device()
+    train_ds = load_data(device)
+    for batch in train_ds:
+        print(batch)
+        break
