2e6dfd80831c96f545728016dd1df940ea3dff49
Zhiyu Guo <zg915@nyu.edu>
2024-11-24 23:40:26 -0500

    enhanced model

--- my_model.py
+++ my_model.py
@@ -8,12 +8,44 @@ class Encoder(nn.Module):
 
     def __init__(self):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(2, 16, kernel_size=3, padding=1) 
-        self.pool1 = nn.MaxPool2d(2) 
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1) 
-        self.pool2 = nn.MaxPool2d(2) 
-        self.relu = nn.ReLU()
-        self.fc = nn.Linear(32 * 16 * 16, 128)
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(2, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2) 
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+
+        self.conv_block4 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+        )
+
+        self.conv_block5 = nn.Sequential(
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.linear_block = nn.Sequential(
+            nn.Linear(16 * 16 * 16, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256)
+        )
+        
 
     def forward(self, x):
         """
@@ -21,16 +53,14 @@ class Encoder(nn.Module):
         Output x: (bs, 32, 16, 16)
         """
         bs, channel, height, width = x.shape # (bs, 2, 65, 65)
-        x = self.conv1(x) # (bs, 16, 65, 65)
-        x = self.relu(x) # (bs, 16, 65, 65)
-        x = self.pool1(x) # (bs, 16, 32, 32)
-
-        x = self.conv2(x) # (bs, 32, 32, 32)
-        x = self.relu(x) # (bs, 32, 32, 322)
-        x = self.pool2(x) # (bs, 32, 16, 16)
-
-        x = self.fc(x.view(bs, -1)) # (bs, 128)
-
+        x = self.conv_block1(x) # (bs, 32, 32, 32)
+        identity = x
+        x = self.conv_block2(x) # (bs, 64, 32, 32)
+        x = self.conv_block3(x) # (bs, 64, 32, 32)
+        x = self.conv_block4(x) # (bs, 32, 32, 32)
+        x = x + identity # (bs, 32, 32, 32)
+        x = self.conv_block5(x) # (bs, 16, 16, 16)
+        x = self.linear_block(x.view(bs, -1)) # (bs, 256)
         return x
     
 
@@ -39,23 +69,30 @@ class Predictor(nn.Module):
 
     def __init__(self):
         super(Predictor, self).__init__()
-        self.fc1 = nn.Linear(130, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.relu = nn.ReLU()
+
+        self.action_embedding = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.ReLU()
+        )
+
+        self.fc1 = nn.Linear(256 + 16, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.leaky_relu = nn.LeakyReLU()
     
-    def forward(self, s, u):
+    def forward(self, state, action):
         """
         Input s:  (bs, 128)
         Input u:  (bs, 2)
         Output x: (bs, 128)
         """
-        x = torch.cat([s, u], dim=1) # (bs, 130)
-        x = self.fc1(x) # (bs, 256)
-        x = self.relu(x) # (bs, 256)
-        x = self.fc2(x) # (bs, 256)
-        x = self.relu(x) # (bs, 256)
-        x = self.fc3(x) # (bs, 128)
+        action = self.action_embedding(action) # (bs, 16)
+        x = torch.cat([state, action], dim=1) # (bs, 256 + 16)
+        x = self.fc1(x) # (bs, 512)
+        x = self.leaky_relu(x) # (bs, 512)
+        x = self.fc2(x) # (bs, 512)
+        x = self.leaky_relu(x) # (bs, 512)
+        x = self.fc3(x) # (bs, 256)
         return x
 
 class JEPA(nn.Module):
@@ -63,26 +100,26 @@ class JEPA(nn.Module):
         super(JEPA, self).__init__()
         self.encoder = Encoder()
         self.predictor = Predictor()
-        self.repr_dim = 128
+        self.repr_dim = 256
 
     def forward(self, states, actions):
         """
         Input states:  (bs, 17, 2, 65, 65)
         Input actions: (bs, 16, 2)
-        Output encoded states:  (17, bs, 128)
-        Output predicted states: (16, bs, 128)
+        Output encoded states:  (17, bs, 256)
+        Output predicted states: (16, bs, 256)
         """
         bs, trajectory_length, channel, height, width = states.shape # (bs, 17, 2, 65, 65)
 
         reshaped_states = states.view(bs * trajectory_length, 2, 65, 65) # (bs * 17, 2, 65, 65)
-        encoded_states = self.encoder(reshaped_states) # (bs * 17, 128)
-        encoded_states = encoded_states.view(bs, trajectory_length, 128).permute(1, 0, 2) # (17, bs, 128)
+        encoded_states = self.encoder(reshaped_states) # (bs * 17, 256)
+        encoded_states = encoded_states.view(bs, trajectory_length, 256).permute(1, 0, 2) # (17, bs, 256)
 
         predicted_states = []
         for i in range(trajectory_length - 1):
-            prediction = self.predictor(encoded_states[i], actions[:, i]) # (bs, 128)
+            prediction = self.predictor(encoded_states[i], actions[:, i]) # (bs, 256)
             predicted_states.append(prediction)
-        predicted_states = torch.stack(predicted_states, dim=0)  # (16, bs, 128)
+        predicted_states = torch.stack(predicted_states, dim=0)  # (16, bs, 256)
 
         return encoded_states, predicted_states
 
