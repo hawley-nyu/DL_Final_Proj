77e6a2f63dd829c9b9db97fb23aff27fd3d88565
Zhiyu Guo <zg915@nyu.edu>
2024-11-25 02:23:51 -0500

    misnamed variables

--- evaluator.py
+++ evaluator.py
@@ -114,7 +114,7 @@ class ProbingEvaluator:
                 # TODO: Forward pass through your model
                 encoded_states, predicted_states = model(batch.states, batch.actions)
                 initial_state = encoded_states[0].unsqueeze(0)  # Take the initial state (T=0) and reshape to (1, BS, D)
-                pred_locs = torch.cat([initial_state, predicted_states], dim=0)
+                pred_encs = torch.cat([initial_state, predicted_states], dim=0)
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
                 ################################################################################
@@ -212,7 +212,7 @@ class ProbingEvaluator:
             # TODO: Forward pass through your model
             encoded_states, predicted_states = model(batch.states, batch.actions)
             initial_state = encoded_states[0].unsqueeze(0)  # Take the initial state (T=0) and reshape to (1, BS, D)
-            pred_locs = torch.cat([initial_state, predicted_states], dim=0)
+            pred_encs = torch.cat([initial_state, predicted_states], dim=0)
 
             # Make sure pred_encs has shape (T, BS, D) at this point
             ################################################################################
