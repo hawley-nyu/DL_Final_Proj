ebbeb56d221c5519387f715052b42354ffc068c8
Zhiyu Guo <zg915@nyu.edu>
2024-11-25 01:55:52 -0500

    print shapes

--- evaluator.py
+++ evaluator.py
@@ -38,6 +38,7 @@ default_config = ProbingConfig()
 
 
 def location_losses(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    print(target.shape, pred.shape)
     assert pred.shape == target.shape
     mse = (pred - target).pow(2).mean(dim=0)
     return mse
