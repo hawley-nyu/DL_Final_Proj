9fa3e405523996d5aa19c5c668e169b9dc5b84ce
Zhiyu Guo <zg915@nyu.edu>
2024-12-02 17:14:30 -0500

    debug

--- my_model.py
+++ my_model.py
@@ -170,12 +170,13 @@ class JEPA(nn.Module):
         """
         bs, trajectory_length, channel, height, width = states.shape # (bs, 17, 2, 65, 65)
 
-        # reshaped_states = states.view(bs * trajectory_length, 2, 65, 65) # (bs * 17, 2, 65, 65)
         states[:,:,1,:,:] = states[:,:,0,:,:]
-        encoded_states = self.encoder(states[:,:-1,:,:,:])
-        encoded_target_states = self.target_encoder(states[:, 1:, :, :, :])
-        # encoded_states = encoded_states.view(bs, trajectory_length, 256).permute(1, 0, 2) # (17, bs, 256)
+        encoded_states = self.encoder(states[:,:-1,:,:,:].view(bs * trajectory_length, 2, 65, 65)) # (bs * 17, 2, 65, 65)
+        encoded_target_states = self.target_encoder(states[:,1:,:,:,:].view(bs * trajectory_length, 2, 65, 65)) 
 
+        encoded_states = encoded_states.view(bs, trajectory_length, 256).permute(1, 0, 2) # (17, bs, 256)
+
+        encoded_target_states = encoded_target_states.view(bs, trajectory_length, 256).permute(1, 0, 2) # (17, bs, 256)
         predicted_states = []
         predicted_states.append(encoded_states[0])
         for i in range(trajectory_length - 1):
