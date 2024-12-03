a12e9978c6e3454667aa343f018e58b5082fc175
Zhiyu Guo <zg915@nyu.edu>
2024-12-03 01:24:20 -0500

    align to merge

--- evaluator.py
+++ evaluator.py
@@ -108,14 +108,11 @@ class ProbingEvaluator:
             batch_size=batch_size,
         )
 
-        for epoch in tqdm(range(epochs), desc=f"Probe prediction epochs"):
-            for batch in tqdm(dataset, desc="Probe prediction step"):
+        for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                encoded_states, predicted_states = model(batch.states, batch.actions)
-                # initial_state = encoded_states[0].unsqueeze(0)  # Take the initial state (T=0) and reshape to (1, BS, D)
-                # pred_encs = torch.cat([initial_state, predicted_states], dim=0)
-                pred_encs = predicted_states
+                pred_encs = model(states=batch.states, actions=batch.actions)
+                pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
                 ################################################################################
@@ -211,10 +208,9 @@ class ProbingEvaluator:
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
             # TODO: Forward pass through your model
-            encoded_states, predicted_states = model(batch.states, batch.actions)
-            # initial_state = encoded_states[0].unsqueeze(0)  # Take the initial state (T=0) and reshape to (1, BS, D)
-            # pred_encs = torch.cat([initial_state, predicted_states], dim=0)
-            pred_encs = predicted_states
+            pred_encs = model(states=batch.states, actions=batch.actions)
+            # # BS, T, D --> T, BS, D
+            pred_encs = pred_encs.transpose(0, 1)
 
             # Make sure pred_encs has shape (T, BS, D) at this point
             ################################################################################
