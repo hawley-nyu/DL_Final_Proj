ed3520c41a3dc88ec38d944dca3b7c72562140fb
Jimmmnny <yuxianwang@Jimmys-MacBook-Air.local>
2024-12-03 21:43:34 -0500

    baseline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dataset import create_wall_dataloader
 from evaluator import ProbingEvaluator
-from train import train_low_energy_two_model
+from VicRegJEPA_Train import train_low_energy_two_model
 import torch
 from models import MockModel
 from models import LowEnergyTwoModel
@@ -9,19 +9,14 @@ import torch.optim as optim
 import argparse
 
 
-def get_device(local=False):
+def get_device():
     """Check for GPU availability."""
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if local:
-        device = torch.device("mps" if torch.backends.mps.is_available() else device)
     print("Using device:", device)
     return device
 
-def load_training_data(device, local=False):
-    if local:
-        data_path="/Users/patrick/data/train"
-    else:
-        data_path="/scratch/DL24FA/train"
+def load_training_data(device):
+    data_path="/scratch/DL24FA/train"
 
     train_ds = create_wall_dataloader(
         data_path=f"{data_path}",
@@ -32,11 +27,8 @@ def load_training_data(device, local=False):
 
     return train_ds
 
-def load_data(device, local=False):
-    if local:
-        data_path="/Users/patrick/data"
-    else:
-        data_path="/scratch/DL24FA"
+def load_data(device):
+    data_path="/scratch/DL24FA"
 
     probe_train_ds = create_wall_dataloader(
         data_path=f"{data_path}/probe_normal/train",
@@ -64,7 +56,7 @@ def load_data(device, local=False):
     return probe_train_ds, probe_val_ds
 
 
-def load_model(device='cuda', local=False):
+def load_model(device='cuda'):
     """Load or initialize the model."""
     # model = MockModel()
     model = LowEnergyTwoModel(device=device, repr_dim=256).to(device)
@@ -92,29 +84,26 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", action="store_true", help="train model and save .pth file")
-    parser.add_argument("--local", action="store_true", help="run on OS X")
     parser.add_argument("--test", action="store_true", help="test mode")
     parser.add_argument("--epochs", type=int, default=1, help="Number of epochs (default: 1)")
     args = parser.parse_args()
 
     num_epochs = args.epochs
     train_only = args.train
-    local = args.local
     test_mode = args.test
     learning_rate = 1e-4
     repr_dim = 256
 
 
-    device = get_device(local=local)
+    device = get_device()
     print(f'Epochs = {num_epochs}')
-    print(f'Local execution = {local}')
     print(f'Learning rate = {learning_rate}')
     print(f'Representation dimension = {repr_dim}')
 
     if train_only:
         print('Training low energy model')
         model = LowEnergyTwoModel(device=device, repr_dim=repr_dim).to(device)
-        train_loader = load_training_data(device=device, local=local) 
+        train_loader = load_training_data(device=device)
 
         predicted_states, target_states = train_low_energy_two_model(
             model=model,
@@ -128,9 +117,9 @@ if __name__ == "__main__":
         print('Saving low energy model in best_model.pth')
         torch.save(model.state_dict(), "best_model.pth")
 
-    else: 
+    else:
         # evaluate the model
         print('Evaluating best_model.pth')
-        probe_train_ds, probe_val_ds = load_data(device, local=local)
-        model = load_model(device=device, local=local)
+        probe_train_ds, probe_val_ds = load_data(device)
+        model = load_model(device=device)
         evaluate_model(device, model, probe_train_ds, probe_val_ds)
